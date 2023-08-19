9b73ca633922fe0a72245a6ca3537a5863764ea2
Pranjul Agrawal <pranjulagrawal9@gmail.com>
2023-08-19 19:35:14 +0530

    Add parent categories to categories

---
+++ src/api/maincategory/controllers/maincategory.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * maincategory controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::maincategory.maincategory');
---
+++ src/api/maincategory/services/maincategory.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * maincategory service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::maincategory.maincategory');
