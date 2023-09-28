943df5d333a9b4f48e308e283ce04ad55cf63c0b
Pranjul Agrawal <pranjulagrawal9@gmail.com>
2023-09-28 14:36:38 +0530

    Add absolute server url to server.js

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  url: env("outfitly-backend-production.up.railway.app"),
+  url: env("ABSOLUTE_SERVER_URL", "http://localhost:1337"),
   app: {
     keys: env.array("APP_KEYS"),
   },
