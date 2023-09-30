bab698b9cc7d922b610f5c3a0ed3d7288100ccaf
Pranjul Agrawal <pranjulagrawal9@gmail.com>
2023-09-30 19:21:52 +0530

    Change upload provider to cloudinary provider in prod.

---
+++ config/env/production/middlewares.js
@@ -0,0 +1,37 @@
+module.exports = [
+  "strapi::errors",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "market-assets.strapi.io",
+            "res.cloudinary.com",
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "market-assets.strapi.io",
+            "res.cloudinary.com",
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  { name: "strapi::body", config: { includeUnparsed: true } },
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
+];
